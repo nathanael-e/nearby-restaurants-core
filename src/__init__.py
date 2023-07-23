6fca7cc691fc338da2fc54b13aed68d782eba284
Nathanael Eneroth <nathanael.eneroth@gmail.com>
2023-07-23 11:09:20 +0200

    dockerfile and pylint

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from . import location
 
+app = Flask(__name__, instance_relative_config=True)
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+
+def create_app():
+    """Create flask and add blueprints"""
     app.register_blueprint(location.bp)
     return app
--- src/location.py
+++ src/location.py
@@ -3,17 +3,18 @@ from flask import Blueprint, request, jsonify
 bp = Blueprint("location", __name__, url_prefix="/api/location")
 
 
-@bp.route("/restaurants", methods=(["GET"]))
+@bp.route("/restaurants", methods=["GET"])
 def restaurants():
+    """Return near restaruants based on longitude and latitude"""
     longitude = request.args.get("longitude")
-    altitude = request.args.get("altitude")
+    altitude = request.args.get("latitude")
 
     if not longitude or not altitude:
-        return jsonify({"error": "Both longitude and altitude are required."}), 400
+        return jsonify({"error": "Both longitude and latitude are required."}), 400
 
     restaurants_data = [
-        {"name": "Restaurant A", "longitude": 123.45, "altitude": 67.89},
-        {"name": "Restaurant B", "longitude": 98.76, "altitude": 54.32},
+        {"name": "Restaurant A", "longitude": 123.45, "latitude": 67.89},
+        {"name": "Restaurant B", "longitude": 98.76, "latitude": 54.32},
     ]
 
     return jsonify(restaurants_data)
