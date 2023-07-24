fbdddc65e4618905103a924c0d419866f30ea8ef
Nathanael Eneroth <nathanael.eneroth@gmail.com>
2023-07-24 09:03:49 +0200

    fix server

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
 from . import location
 
-app = Flask(__name__, instance_relative_config=True)
-
 
 def create_app():
     """Create flask and add blueprints"""
+    app = Flask(__name__, instance_relative_config=True)
     app.register_blueprint(location.bp)
     return app
