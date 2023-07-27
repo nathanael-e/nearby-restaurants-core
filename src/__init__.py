212c8966bf4755f78729b71e85be38d3114b7e82
Nathanael Eneroth <nathanael.eneroth@gmail.com>
2023-07-27 10:54:29 +0200

    add google places

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 import os
 from itsdangerous import Serializer
 from flask import Flask
+from . import constants
 from .views import app_restfinder_info
 from .views.location_api import LocationAPI
 
@@ -8,10 +9,16 @@ from .views.location_api import LocationAPI
 class FlaskServer:
     def __init__(self):
         self.app = Flask(__name__, instance_relative_config=True)
-        api_secret = os.getenv("API_SECRET")
+        api_secret = os.getenv(constants.API_TOKEN)
+        google_places_api_sercet = os.getenv(constants.API_GOOGLE_PLACES_TOKEN)
         if not api_secret:
-            raise ValueError("Environment variable 'API_SECRET' is null")
-        self.app.config["SECRET_KEY"] = api_secret
+            raise ValueError(f"Environment variable '{constants.API_TOKEN}' is null")
+        if not google_places_api_sercet:
+            raise ValueError(
+                f"Environment variable '{constants.API_GOOGLE_PLACES_TOKEN}' is null"
+            )
+        self.app.config[constants.API_TOKEN] = api_secret
+        self.app.config[constants.API_GOOGLE_PLACES_TOKEN] = google_places_api_sercet
         self.__init()
 
     def __init(self):
--- test/test_location_api.py
+++ test/test_location_api.py
@@ -1,6 +1,9 @@
 # pylint: disable=C0116
 from test.test_base import TestBase
+import re
+import json
 from flask import Flask
+import requests_mock
 
 
 class TestLocationAPI(TestBase):
@@ -24,6 +27,47 @@ class TestLocationAPI(TestBase):
             data = response.json
             assert response.status_code == 401
             assert data["message"] == "This API requires an access token."
+
+    def test_location_api_invalid_google_access_token(
+        self, test_client: Flask.test_client, headers: dict
+    ):
+        with requests_mock.Mocker() as mocker:
+            with open(
+                "test/resources/google_places_invalid_access_token.json",
+                encoding="utf-8",
+            ) as response_json:
+                mocker = requests_mock.Mocker()
+                matcher = re.compile(
+                    "https://maps.googleapis.com/maps/api/place/nearbysearch/.*"
+                )
+                mocker.get(
+                    matcher,
+                    json=json.load(response_json),
+                )
+                mocker.start()
+                response = test_client.get(
+                    "/api/location/restaurants?longitude=2&latitude=5",
+                    headers=headers,
+                )
+                data = response.json
+                assert response.status_code == 400
+                assert data == "error: received invalid status code: REQUEST_DENIED"
+
+    def test_location_api_bad_request(
+        self, test_client: Flask.test_client, headers: dict
+    ):
+        with requests_mock.Mocker() as mocker:
+            mocker = requests_mock.Mocker()
+            matcher = re.compile(
+                "https://maps.googleapis.com/maps/api/place/nearbysearch/.*"
+            )
+            mocker.get(matcher, status_code=400)
+            mocker.start()
+            response = test_client.get(
+                "/api/location/restaurants?longitude=2&latitude=5",
+                headers=headers,
+            )
+            assert response.status_code == 500
 
     def test_location_api_invalid_token(self, app):
         invalid_header = {
